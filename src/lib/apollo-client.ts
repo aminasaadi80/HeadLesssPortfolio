ca572d2d962a30c47aba164c27fff0b05232a0d0
Amin <asadiamin434@gmail.com>
2025-08-28 17:23:49 +0330

    fix api server address

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'https://cms.amincharkh.com/admin/graphql',
+  uri: 'https://cms.aminasaadi.ir/admin/graphql',
 });
 
 export const client = new ApolloClient({
